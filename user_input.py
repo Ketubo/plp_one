34a3559cba3c93a39c466913835f6eb7fbd42f31
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-08-19 22:23:09 +0300

    User details

---
+++ user_input.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+
+name = input('Please input your name: ')
+age = input("Please input your age in number: ")
+location = input("Where do you live? ")
+
+print(f"Hello {name}! You are {age} years old and live in {location}")
